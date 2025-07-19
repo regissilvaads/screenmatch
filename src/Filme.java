7da893ee0218eae23540637ed045b1a9b17f941c
Regis <regis.silva.ads@gmail.com>
2025-07-19 13:29:53 -0300

    Adicionando novos  métodos para Classe Filme(exibeFichaTecnica, avaliaca, calculaMedia)

--- src/Filme.java
+++ src/Filme.java
@@ -2,6 +2,21 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
+    double somaAvaliacao;
     int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+    void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + this.nome);
+        System.out.println("Ano de lançamento: " + this.anoDeLancamento);
+    }
+
+    void avalia(double nota) {
+        this.somaAvaliacao += nota;
+        this.totalDeAvaliacoes++;
+    }
+
+    double calculaMedia() {
+        return this.somaAvaliacao / this.totalDeAvaliacoes;
+    }
 }
